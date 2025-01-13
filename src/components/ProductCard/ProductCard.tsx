01270a02b500940078eefa593245cb1ab8a3db75
SvitlanaRamanauskas <ramanauskassvs@gmail.com>
2025-01-13 15:08:28 +0200

    photo check

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -20,7 +20,7 @@ export const ProductCard: React.FC<Props> = ({ item, vyshyvankyFromServer }) => 
           state={{ vyshyvankyFromServer }}
         >
           <img
-            src={`/${item.image}`}
+            src={`${item.image}`}
             alt="productCard"
             className="card__image"
           />
